966a71116b807c2b43d40015a452a90dc2f5f378
gotodk <gotodk@sina.com>
2016-04-27 16:28:09 +0800

    xcvcxvcv

---
+++ ws核心业务中心/App_Code/NoReSetClass/NoReSet_160427000034.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using FMDBHelperClass;
+using FMipcClass;
+using System.Collections;
+using System.Data;
+using FMPublicClass;
+using System.Numerics;
+using System.Web.Script.Serialization;
+
+public class NoReSet_160427000034
+{
+ 
+
+    /// <summary>
+    /// 初始化返回值数据集,执行结果只有两种ok和err(大多数情况是这个标准)
+    /// </summary>
+    /// <returns></returns>
+    private DataSet initReturnDataSet()
+    {
+        DataSet ds = new DataSet();
+        DataTable auto2 = new DataTable();
+        auto2.TableName = "返回值单条";
+        auto2.Columns.Add("执行结果");
+        auto2.Columns.Add("提示文本");
+        auto2.Columns.Add("附件信息1");
+        auto2.Columns.Add("附件信息2");
+        auto2.Columns.Add("附件信息3");
+        auto2.Columns.Add("附件信息4");
+        auto2.Columns.Add("附件信息5");
+        ds.Tables.Add(auto2);
+        return ds;
+    }
+
+    /// <summary>
+    /// 增加数据
+    /// </summary>
+    /// <param name="parameter_forUI">前台表单传来的参数</param>
+    /// <returns></returns>
+    public DataSet NRS_ADD(DataTable parameter_forUI)
+    {
+        //接收转换参数
+        Hashtable ht_forUI = new Hashtable();
+        for (int i = 0; i < parameter_forUI.Rows.Count; i++)
+        {
+            ht_forUI[parameter_forUI.Rows[i]["参数名"].ToString()] = parameter_forUI.Rows[i]["参数值"].ToString();
+        }
+        //初始化返回值
+        DataSet dsreturn = initReturnDataSet().Clone();
+        dsreturn.Tables["返回值单条"].Rows.Add(new string[] { "err", "初始化" });
+        //参数合法性各种验证，这里要根据具体业务逻辑处理
+
+        //开始真正的处理，根据业务逻辑操作数据库
+        I_Dblink I_DBL = (new DBFactory()).DbLinkSqlMain("");
+        Hashtable return_ht = new Hashtable();
+        ArrayList alsql = new ArrayList();
+        Hashtable param = new Hashtable();
+        //以可排序guid方式生成
+        //string guid = CombGuid.GetNewCombGuid("D"); 
+        //以两位年+两位月+两位日+6位序列顺序号方式生成
+        string guid = ht_forUI["HID"].ToString();
+        param.Add("@HID", guid);
+        param.Add("@H_YYID", ht_forUI["H_YYID"].ToString());
+        param.Add("@Eleixing", ht_forUI["Eleixing"].ToString());
+        param.Add("@Hqianshuren", ht_forUI["Hqianshuren"].ToString());
+        param.Add("@Hzongjia", ht_forUI["Hzongjia"].ToString());
+        param.Add("@Hqianshuriqi", ht_forUI["Hqianshuriqi"].ToString());
+        param.Add("@Hfukuanriqi", ht_forUI["Hfukuanriqi"].ToString());
+        param.Add("@Hshouuanjin", ht_forUI["Hshouuanjin"].ToString());
+
+        param.Add("@Hfukuanzhouqi", ht_forUI["Hfukuanzhouqi"].ToString());
+        param.Add("@Hshifouzhiqu", ht_forUI["Hshifouzhiqu"].ToString());
+        param.Add("@Hbeizhu", ht_forUI["Hbeizhu"].ToString());
+
+        alsql.Add("INSERT INTO ZZZ_HTGL(HID, H_YYID, Eleixing, Hqianshuren, Hzongjia, Hqianshuriqi, Hfukuanriqi, Hshouuanjin, Hfukuanzhouqi,   Hshifouzhiqu, Hbeizhu) VALUES(@HID, @H_YYID, @Eleixing, @Hqianshuren, @Hzongjia, @Hqianshuriqi, @Hfukuanriqi, @Hshouuanjin, @Hfukuanzhouqi,   @Hshifouzhiqu, @Hbeizhu)");
+
+ 
+        //遍历子表， 插入 
+        string zibiao_gts_id = "grid-table-subtable-160427000636";
+        DataTable subdt = jsontodatatable.ToDataTable(ht_forUI[zibiao_gts_id].ToString());
+        //必须验证js脚本获取的数量和c#反序列化获取的数量一致才能继续。防止出错
+        if (ht_forUI[zibiao_gts_id + "_fcjsq"].ToString() != subdt.Rows.Count.ToString())
+        {
+            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "err";
+            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "子表数据量与获取量不相符，系统出现问题。";
+            return dsreturn;
+        }
+
+        param.Add("@sub_" + "MainID", guid); //隶属主表id
+ 
+        for (int i = 0; i < subdt.Rows.Count; i++)
+        {
+            param.Add("@sub_" + "id" + "_" + i, CombGuid.GetMewIdFormSequence("ZZZ_HTGL_SB"));
+
+            param.Add("@sub_" + "HSB_SID" + "_" + i, subdt.Rows[i]["设备编号"].ToString());
+            param.Add("@sub_" + "HSBbegin" + "_" + i, subdt.Rows[i]["生效日期"].ToString());
+            param.Add("@sub_" + "HSBend" + "_" + i, subdt.Rows[i]["失效日期"].ToString());
+            param.Add("@sub_" + "HSBjiage" + "_" + i, subdt.Rows[i]["金额"].ToString());
+    
+
+
+            string INSERTsql = "INSERT INTO ZZZ_HTGL_SB ( HSBID, HSB_HID,  HSB_SID,   HSBbegin,  HSBend, HSBjiage  ) VALUES(@sub_" + "id" + "_" + i + ", @sub_MainID, @sub_"+ "HSB_SID" + "_" + i + ", @sub_" + "HSBbegin" + "_" + i + ", @sub_" + "HSBend" + "_" + i + ", @sub_" + "HSBjiage" + "_" + i + "  )";
+            alsql.Add(INSERTsql);
+        }
+
+        return_ht = I_DBL.RunParam_SQL(alsql, param);
+
+        if ((bool)(return_ht["return_float"]))
+        {
+            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "ok";
+            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "新增成功！";
+        }
+        else
+        {
+            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "err";
+            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "系统故障，保存失败：" + return_ht["return_errmsg"].ToString();
+        }
+        return dsreturn;
+    }
+
+    /// <summary>
+    /// 编辑数据
+    /// </summary>
+    /// <param name="parameter_forUI">前台表单传来的参数</param>
+    /// <returns></returns>
+    public DataSet NRS_EDIT(DataTable parameter_forUI)
+    {
+        //接收转换参数
+        Hashtable ht_forUI = new Hashtable();
+        for (int i = 0; i < parameter_forUI.Rows.Count; i++)
+        {
+            ht_forUI[parameter_forUI.Rows[i]["参数名"].ToString()] = parameter_forUI.Rows[i]["参数值"].ToString();
+        }
+
+
+        //初始化返回值
+        DataSet dsreturn = initReturnDataSet().Clone();
+        dsreturn.Tables["返回值单条"].Rows.Add(new string[] { "err", "初始化" });
+
+        //参数合法性各种验证，这里省略
+        if (ht_forUI["idforedit"].ToString().Trim() == "")
+        {
+            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "err";
+            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "没有明确的修改目标！";
+            return dsreturn;
+        }
+        //开始真正的处理，这里只是演示，所以直接在这里写业务逻辑代码了
+
+        I_Dblink I_DBL = (new DBFactory()).DbLinkSqlMain("");
+        Hashtable return_ht = new Hashtable();
+        ArrayList alsql = new ArrayList();
+        Hashtable param = new Hashtable();
+        param.Add("@HID", ht_forUI["idforedit"].ToString());
+        param.Add("@H_YYID", ht_forUI["H_YYID"].ToString());
+        param.Add("@Eleixing", ht_forUI["Eleixing"].ToString());
+        param.Add("@Hqianshuren", ht_forUI["Hqianshuren"].ToString());
+        param.Add("@Hzongjia", ht_forUI["Hzongjia"].ToString());
+        param.Add("@Hqianshuriqi", ht_forUI["Hqianshuriqi"].ToString());
+        param.Add("@Hfukuanriqi", ht_forUI["Hfukuanriqi"].ToString());
+        param.Add("@Hshouuanjin", ht_forUI["Hshouuanjin"].ToString());
+
+        param.Add("@Hfukuanzhouqi", ht_forUI["Hfukuanzhouqi"].ToString());
+        param.Add("@Hshifouzhiqu", ht_forUI["Hshifouzhiqu"].ToString());
+        param.Add("@Hbeizhu", ht_forUI["Hbeizhu"].ToString());
+
+        alsql.Add("UPDATE ZZZ_HTGL SET   H_YYID=@H_YYID, Eleixing=@Eleixing, Hqianshuren=@Hqianshuren, Hzongjia=@Hzongjia, Hqianshuriqi=@Hqianshuriqi, Hfukuanriqi=@Hfukuanriqi, Hshouuanjin=@Hshouuanjin, Hfukuanzhouqi=@Hfukuanzhouqi,   Hshifouzhiqu=@Hshifouzhiqu, Hbeizhu=@Hbeizhu where HID=@HID ");
+
+
+        //遍历子表，先删除，再插入，已有主键的不重新生成。
+        string zibiao_gts_id = "grid-table-subtable-160427000636";
+        DataTable subdt = jsontodatatable.ToDataTable(ht_forUI[zibiao_gts_id].ToString());
+        //必须验证js脚本获取的数量和c#反序列化获取的数量一致才能继续。防止出错
+        if (ht_forUI[zibiao_gts_id + "_fcjsq"].ToString() != subdt.Rows.Count.ToString())
+        {
+            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "err";
+            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "子表数据量与获取量不相符，系统出现问题。";
+            return dsreturn;
+        }
+        param.Add("@sub_" + "MainID", ht_forUI["idforedit"].ToString()); //隶属主表id
+        alsql.Add("delete ZZZ_HTGL_SB where  HSB_HID = @sub_" + "MainID");
+        for (int i = 0; i < subdt.Rows.Count; i++)
+        {
+            if (subdt.Rows[i]["隐藏编号"].ToString().Trim() == "")
+            {
+                param.Add("@sub_" + "id" + "_" + i,   CombGuid.GetMewIdFormSequence("ZZZ_HTGL_SB"));
+            }
+            else
+            {
+                param.Add("@sub_" + "id" + "_" + i, subdt.Rows[i]["隐藏编号"].ToString());
+            }
+
+
+            param.Add("@sub_" + "HSB_SID" + "_" + i, subdt.Rows[i]["设备编号"].ToString());
+            param.Add("@sub_" + "HSBbegin" + "_" + i, subdt.Rows[i]["生效日期"].ToString());
+            param.Add("@sub_" + "HSBend" + "_" + i, subdt.Rows[i]["失效日期"].ToString());
+            param.Add("@sub_" + "HSBjiage" + "_" + i, subdt.Rows[i]["金额"].ToString());
+
+
+            string INSERTsql = "INSERT INTO ZZZ_HTGL_SB ( HSBID, HSB_HID,  HSB_SID,   HSBbegin,  HSBend, HSBjiage  ) VALUES(@sub_" + "id" + "_" + i + ", @sub_MainID, @sub_" + "HSB_SID" + "_" + i + ", @sub_" + "HSBbegin" + "_" + i + ", @sub_" + "HSBend" + "_" + i + ", @sub_" + "HSBjiage" + "_" + i + "  )";
+            alsql.Add(INSERTsql);
+        }
+
+
+        return_ht = I_DBL.RunParam_SQL(alsql, param);
+
+
+
+
+        if ((bool)(return_ht["return_float"]))
+        {
+
+            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "ok";
+            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "修改成功！";
+        }
+        else
+        {
+            //其实要记录日志，而不是输出，这里只是演示
+            //dsreturn.Tables.Add(parameter_forUI.Copy());
+            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "err";
+            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "系统故障，修改失败：" + return_ht["return_errmsg"].ToString();
+        }
+
+
+
+
+
+        return dsreturn;
+    }
+
+    /// <summary>
+    /// 编辑数据前获取数据
+    /// </summary>
+    /// <param name="parameter_forUI">前台表单传来的参数</param>
+    /// <returns></returns>
+    public DataSet NRS_EDIT_INFO(DataTable parameter_forUI)
+    {
+        //接收转换参数
+        Hashtable ht_forUI = new Hashtable();
+        for (int i = 0; i < parameter_forUI.Rows.Count; i++)
+        {
+            ht_forUI[parameter_forUI.Rows[i]["参数名"].ToString()] = parameter_forUI.Rows[i]["参数值"].ToString();
+        }
+
+
+        //初始化返回值
+        DataSet dsreturn = initReturnDataSet().Clone();
+        dsreturn.Tables["返回值单条"].Rows.Add(new string[] { "err", "初始化" });
+
+        //参数合法性各种验证，这里省略
+
+        //开始真正的处理，这里只是演示，所以直接在这里写业务逻辑代码了
+
+        I_Dblink I_DBL = (new DBFactory()).DbLinkSqlMain("");
+        Hashtable return_ht = new Hashtable();
+        Hashtable param = new Hashtable();
+        param.Add("@HID", ht_forUI["idforedit"].ToString());
+
+        return_ht = I_DBL.RunParam_SQL("select top 1 * from View_ZZZ_HTGL_list where HID=@HID", "数据记录", param);
+
+        if ((bool)(return_ht["return_float"]))
+        {
+            DataTable redb = ((DataSet)return_ht["return_ds"]).Tables["数据记录"].Copy();
+
+            if (redb.Rows.Count < 1)
+            {
+                dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "err";
+                dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "没有找到指定数据!";
+                return dsreturn;
+            }
+
+            dsreturn.Tables.Add(redb);
+
+
+            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "ok";
+            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "";
+        }
+        else
+        {
+            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "err";
+            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "意外错误，获取失败：" + return_ht["return_errmsg"].ToString();
+        }
+
+
+        return dsreturn;
+    }
+
+
+}
---
+++ ws界面中心/adminht/corepage/bas/edit_htgl.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/adminht/MasterPageMain.master" AutoEventWireup="true" CodeFile="edit_htgl.aspx.cs" Inherits="bas_edit_htgl" %>
+
+<%@ Register Src="~/pucu/wuc_css.ascx" TagPrefix="uc1" TagName="wuc_css" %>
+<%@ Register Src="~/pucu/wuc_content.ascx" TagPrefix="uc1" TagName="wuc_content" %>
+<%@ Register Src="~/pucu/wuc_script.ascx" TagPrefix="uc1" TagName="wuc_script" %>
+
+
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="sp_head" runat="Server">
+    <!-- 往模板页附加的head内容 -->
+    <uc1:wuc_css runat="server" ID="wuc_css" />
+
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="sp_daohang" runat="Server">
+    <!-- 附加的本页导航栏内容 -->
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="sp_pagecontent" runat="Server">
+    <!-- 附加的右侧主要功能切换区内容,不含导航 -->
+    <uc1:wuc_content runat="server" ID="wuc_content"  />
+ 
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="sp_script" runat="Server">
+
+       <!-- 附加的body底部本页专属的自定义js脚本 -->
+    <uc1:wuc_script runat="server" ID="wuc_script" />
+        <!-- 选择产品编号后，自动带入一些数据 -->
+    <script type="text/javascript">
+             jQuery(function ($) {
+                
+
+
+                 //隐藏子表弹窗共享字段
+                 $("#yincang_sblb").closest(".form-group").hide();
+
+                 if (getUrlParam("fff") == "1") {
+
+                     $("#HID").attr("readonly", "readonly");
+                 }
+
+                 var dfx_str_kh = "#show_searchopenyhbspgogo_H_YYID";
+                 var oldzhi_kh = $(dfx_str_kh).text();
+            
+                 var jiancha_YYID = window.setInterval(function () {
+
+                     //弹窗特殊条件
+                     //$("#Hbeizhu").val($("#searchopenyhbspgogo_subtcid_HSB_SID").attr("teshuwhere"));
+                     var guolv = $("#H_YYID").val();
+                     $("#searchopenyhbspgogo_subtcid_HSB_SID").attr("teshuwhere", "S_YYID='" + guolv + "'");
+
+                     //带入字段
+                     if ($(dfx_str_kh).text() != oldzhi_kh) {
+                         var re = /\[.*?\]/ig;
+                         var arr = $(dfx_str_kh).text().match(re);
+
+                         if (arr != null) {//如果能匹配成功即arr数组不为空，循环输出结果
+                             for (var i = 0; i < arr.length; i++) {
+                                 var arr_z = arr[i].split(':');
+                                 if (arr_z[0] == "[客户名称")
+                                 { $("#YYname").val($.trim(arr_z[1]).replace("]", "")); }
+
+                             }
+                         }
+
+                         oldzhi = $(dfx_str).text();
+                     }
+                 }, 500);
+ 
+        });
+        </script>
+</asp:Content>
+
