ff39fb9cfdf7d077e8aeddc6ec948bfc00e1af8c
gotodk <gotodk@sina.com>
2016-11-01 13:43:20 +0800

    werwer

--- ws核心业务中心/App_Code/NoReSetClass/NoReSet_160923000060.cs
+++ ws核心业务中心/App_Code/NoReSetClass/NoReSet_160923000060.cs
@@ -33,6 +33,43 @@ public class NoReSet_160923000060
         return ds;
     }
 
+
+
+    /// <summary>
+    /// 检查单据状态
+    /// </summary>
+    /// <returns></returns>
+    public string check_zhuangtai(string FCID)
+    {
+
+        I_Dblink I_DBL = (new DBFactory()).DbLinkSqlMain("");
+        Hashtable return_ht = new Hashtable();
+        Hashtable param = new Hashtable();
+        param.Add("@FCID", FCID);
+
+        return_ht = I_DBL.RunParam_SQL("select top 1 FCzhuangtai from ZZZ_xiaoshoufahuo where FCID=@FCID", "数据记录", param);
+
+        if ((bool)(return_ht["return_float"]))
+        {
+            DataTable redb = ((DataSet)return_ht["return_ds"]).Tables["数据记录"].Copy();
+
+            if (redb.Rows.Count < 1)
+            {
+                return "";
+            }
+            else
+            {
+                return redb.Rows[0]["FCzhuangtai"].ToString();
+            }
+        }
+        else
+        {
+            return "";
+        }
+
+    }
+
+
     /// <summary>
     /// 增加数据
     /// </summary>
@@ -72,15 +109,102 @@ public class NoReSet_160923000060
         }
         //开始真正的处理，这里只是演示，所以直接在这里写业务逻辑代码了
 
-        dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "ok";
-        dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "生成单据成功！{"+ ht_forUI["idforedit"].ToString() + "}";
-        return dsreturn;
+ 
 
-        //检查单据状态是否允许生成eas单据，只有审核状态才能生成
-
+        //检查单据状态是否允许生成eas单据，只有审核状态才能生成，并返回发货单需要的数据
+        if (check_zhuangtai(ht_forUI["idforedit"].ToString().Trim()) != "审核")
+        {
+            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "err";
+            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "生成失败，只有审核状态才允许生成。";
+            return dsreturn;
+        }
         //根据表单，生成接口需要的数据集
 
+        DataSet dsmain = new DataSet();
+        DataTable dbHead = new DataTable();
+        dbHead.TableName = "dbHead";
+        dbHead.Columns.Add("本地单号", typeof(string));
+        dbHead.Columns.Add("事务类型", typeof(string));
+        dbHead.Columns.Add("成本中心", typeof(string));
+        dbHead.Columns.Add("部门", typeof(string));
+        dbHead.Columns.Add("制单人", typeof(string));
+        dbHead.Columns.Add("销售方式", typeof(string));
+        dbHead.Columns.Add("销售组织", typeof(string));
+        dbHead.Columns.Add("币别", typeof(string));
+        dbHead.Columns.Add("汇率", typeof(string));
+        dbHead.Columns.Add("订货客户", typeof(string));
+        dbHead.Columns.Add("付款类型", typeof(string));
+        dbHead.Columns.Add("库存组织", typeof(string));
+        dbHead.Columns.Add("控制单元", typeof(string));
+        dbHead.Columns.Add("业务类型", typeof(string));
+        dsmain.Tables.Add(dbHead);
+        DataTable dbEntry = new DataTable();
+        dbEntry.TableName = "dbEntry";
+        dbEntry.Columns.Add("物料编码", typeof(string));
+        dbEntry.Columns.Add("物料类别", typeof(string));
+        dbEntry.Columns.Add("发货数量", typeof(string));
+        dbEntry.Columns.Add("批号", typeof(string));
+        dbEntry.Columns.Add("单价", typeof(string));
+        dbEntry.Columns.Add("子表主键", typeof(string));
+        dsmain.Tables.Add(dbEntry);
+
+        dsmain.Tables["dbHead"].Rows.Add(new string[]{ "", "", "", "", "", "", "", "", "", "", "", "", "", "" });
+        dsmain.Tables["dbHead"].Rows[0]["本地单号"] = ht_forUI["idforedit"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["事务类型"] = ht_forUI["shiwuleixing"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["成本中心"] = ht_forUI["chengbenzhongxin"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["部门"] = ht_forUI["bumen"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["制单人"] = ht_forUI["zhidanren"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["销售方式"] = ht_forUI["xiaoshoufangshi"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["销售组织"] = ht_forUI["xiaoshouzuzhi"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["币别"] = ht_forUI["bibie"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["汇率"] = ht_forUI["huilv"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["订货客户"] = ht_forUI["FC_YYID"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["付款类型"] = ht_forUI["fukuanfangshi"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["库存组织"] = ht_forUI["kucunzuzhi"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["控制单元"] = ht_forUI["kongzhidanyuan"].ToString();
+        dsmain.Tables["dbHead"].Rows[0]["业务类型"] = ht_forUI["yewuleixing"].ToString();
+ 
+        string zibiao_gts_id = "grid-table-subtable-160923000244";
+        DataTable subdt = jsontodatatable.ToDataTable(ht_forUI[zibiao_gts_id].ToString());
+        //必须验证js脚本获取的数量和c#反序列化获取的数量一致才能继续。防止出错
+        if (ht_forUI[zibiao_gts_id + "_fcjsq"].ToString() != subdt.Rows.Count.ToString())
+        {
+            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "err";
+            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "子表数据量与获取量不相符，系统出现问题。";
+            return dsreturn;
+        }
+        for (int i = 0; i < subdt.Rows.Count; i++)
+        {
+            dsmain.Tables["dbEntry"].Rows.Add(new string[] {  subdt.Rows[i]["物料编码"].ToString(), subdt.Rows[i]["物料类别"].ToString(), subdt.Rows[i]["发货数量"].ToString(), subdt.Rows[i]["设备档案序列号"].ToString(), subdt.Rows[i]["单价"].ToString(), subdt.Rows[i]["编号"].ToString() });
+ 
+ 
+        }
+
         //调用接口生成表单
+        DataSet dsre = new DataSet();
+        object[] re_dsi = IPC.Call("EAS出库单小伟生成", new object[] { dsmain });
+        if (re_dsi[0].ToString() == "ok" && re_dsi[1] != null)
+        {
+             dsre = (DataSet)(re_dsi[1]);
+            //            3、	返回数据DataSet包含三个DataTable(0, 1, 2)
+            //4、	Datatable(0):一行一列，存储返回提示文本内容。
+            //5、	Datatable(1)：单据头。
+            //6、	Datatable(2)：单据分录，可能会变更分录“批号”。
+            if (dsre.Tables[0].Rows[0][0].ToString().IndexOf("单据保存成功") < 0)
+            {
+                dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "err";
+                dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "错误err，接口调用成功，但接口返回：" + dsre.Tables[0].Rows[0][0].ToString();
+                return dsreturn;
+            }
+            
+            
+        }
+        else
+        {
+            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "err";
+            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "错误err，接口调用失败。" + re_dsi[1].ToString();
+            return dsreturn;
+         }
 
         //获取接口执行结果，反写本地子表序列号字段
 
@@ -92,53 +216,16 @@ public class NoReSet_160923000060
         Hashtable return_ht = new Hashtable();
         ArrayList alsql = new ArrayList();
         Hashtable param = new Hashtable();
-        param.Add("@id", ht_forUI["idforedit"].ToString());
-        param.Add("@fieldtest", ht_forUI["fieldtest"].ToString());
 
-        alsql.Add("UPDATE FUP_FormsDemoDB SET fieldtest = @fieldtest where id=@id ");
-
-
-        //遍历子表，先删除，再插入，已有主键的不重新生成。
-        string zibiao_gts_id = "grid-table-subtable-160923000244";
-        DataTable subdt = jsontodatatable.ToDataTable(ht_forUI[zibiao_gts_id].ToString());
-        //必须验证js脚本获取的数量和c#反序列化获取的数量一致才能继续。防止出错
-        if (ht_forUI[zibiao_gts_id + "_fcjsq"].ToString() != subdt.Rows.Count.ToString())
+        //循环返回的子表数据，更新序列号
+        for (int i = 0; i < dsre.Tables[2].Rows.Count; i++)
         {
-            dsreturn.Tables["返回值单条"].Rows[0]["执行结果"] = "err";
-            dsreturn.Tables["返回值单条"].Rows[0]["提示文本"] = "子表数据量与获取量不相符，系统出现问题。";
-            return dsreturn;
+            param.Add("@sub_" + "FCscsbxlh" + "_" + i, dsre.Tables[2].Rows[i]["批号"].ToString());
+            param.Add("@sub_" + "FCSID" + "_" + i, dsre.Tables[2].Rows[i]["子表主键"].ToString());
+            string upupsql = "update ZZZ_xiaoshoufahuo_sb set FCscsbxlh="+ "@sub_" + "FCscsbxlh" + "_" + i + " where FCSID="+ "@sub_" + "FCscsbxlh" + "_" + i+"";
+            alsql.Add(upupsql);
         }
-        param.Add("@sub_" + "MainID", ht_forUI["idforedit"].ToString()); //隶属主表id
-        alsql.Add("delete demouser_sub_test where  SID = @sub_" + "MainID");
-        for (int i = 0; i < subdt.Rows.Count; i++)
-        {
-            if (subdt.Rows[i]["隐藏编号"].ToString().Trim() == "")
-            {
-                param.Add("@sub_" + "id" + "_" + i,   CombGuid.GetMewIdFormSequence("demouser_sub_test"));
-            }
-            else
-            {
-                param.Add("@sub_" + "id" + "_" + i, subdt.Rows[i]["隐藏编号"].ToString());
-            }
-
-
-            param.Add("@sub_" + "Sname" + "_" + i, subdt.Rows[i]["姓名"].ToString());
-            param.Add("@sub_" + "Scity" + "_" + i, subdt.Rows[i]["城市"].ToString());
-            param.Add("@sub_" + "Sint" + "_" + i, subdt.Rows[i]["整数"].ToString());
-            param.Add("@sub_" + "Sdecimal" + "_" + i, subdt.Rows[i]["小数"].ToString());
-            param.Add("@sub_" + "CreateTime" + "_" + i, subdt.Rows[i]["添加日期"].ToString());
-
-
-            string INSERTsql = "INSERT INTO demouser_sub_test ( id, SID, Sname,   Scity,  Sint, Sdecimal,CreateTime ) VALUES(@sub_" + "id" + "_" + i + ", @sub_MainID, @sub_" + "Sname" + "_" + i + ", @sub_" + "Scity" + "_" + i + ", @sub_" + "Sint" + "_" + i + ", @sub_" + "Sdecimal" + "_" + i + ", @sub_" + "CreateTime" + "_" + i + "  )";
-            alsql.Add(INSERTsql);
-        }
-
-
         return_ht = I_DBL.RunParam_SQL(alsql, param);
-
-
-
-
         if ((bool)(return_ht["return_float"]))
         {
 
